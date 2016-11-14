33c4afcfe1e11c4ee1b014f79a7bc4427750010d
nbshelton <nickbshelton@gmail.com>
2016-11-14 15:25:05 -0500

    Initial commit

---
+++ com.wikipedia.url.js
@@ -0,0 +1,52 @@
+exports = module.exports = {
+	id : 'com.wikipedia.url',
+	input : ["location"],
+	output : ["wikipedia_url"],
+	input_optional : [],
+	output_optional : [],
+	meta : {
+		tags : [ 'location' ],
+		author : 'unknown'
+	},
+	process : function(req)
+	{
+		var _id = this.id;
+
+		var params =
+		{
+			format : 'json',
+			action : 'query',
+			titles : req.location,
+			prop : 'info',
+			inprop : 'url',
+			indexpageids : ''
+		};
+
+		this.request({
+			method: 'GET',
+			url: "http://en.wikipedia.org/w/api.php",
+			qs: params,
+			json: true
+		}, 
+		function(err, res, obj)
+		{
+			if (!err && res.statusCode == 200)
+			{
+				var thispage = obj.query.pageids[0];
+				if ( Number( thispage ) >= 0 ) // dh : -1 indicates there is not yet a page with the returned url
+				{
+					var mut = 
+					{
+						source : _id,
+						wikipedia_url : obj.query.pages[thispage].fullurl
+					};
+					udu.mutateRequest(req, mut);
+				} else {
+					udu.addFailureToRequest(req, _id);
+				}
+			} else {
+				udu.addFailureToRequest(req, _id);
+			}
+		});
+	}
+}
